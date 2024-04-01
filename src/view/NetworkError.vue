edf94bf0687901a092110d14c762c44483465fdf
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-04-01 15:16:56 +0200

    lecture11-end

--- src/view/NetworkError.vue
+++ src/view/NetworkError.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="networkError">
-        <h1>Uh-Oh!</h1>
-
-        <h3>
+      <h1>Uh-Oh!</h1>
+  
+      <h3>
         It looks like you're experiencing some network issues, please take a
         breath and <a href="#" @click="$router.go(-1)">click here</a> to try
         again.
-        </h3>
+      </h3>
     </div>
-</template>
+  </template>
